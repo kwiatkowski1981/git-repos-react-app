846a2f92c86dff560bec6a31422234d9f2098bb4
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2022-03-28 19:23:52 +0200

    na commity czas przyjdzie :)

--- src/components/ApiCard.js
+++ src/components/ApiCard.js
@@ -11,31 +11,31 @@ export const ApiCard = props => {
     let color = initialColor;
     let containerBackgroundOpacity = 0.55;
     let technologyTextOpacity = 1;
-    const technologyUsedColor = (usedLanguage, opacity) => {
+    const technologyUsedColor = (usedLanguage, colorModifier, opacity) => {
         if (usedLanguage === 'Java') {
-            color = `rgb(168,116,47, ${opacity})`
+            color = `rgb(168, 116, ${47 + colorModifier}, ${opacity})`
         }
         else if (usedLanguage === 'JavaScript') {
-            color = `rgb(239,223,112, ${opacity})`
+            color = `rgb(239, 223, ${112 + colorModifier}, ${opacity})`
             console.log(color);
         }
         else if (usedLanguage === 'HTML') {
-            color = `rgb(211,86,53, ${opacity})`
+            color = `rgb(211, 86, ${53 + colorModifier}, ${opacity})`
         }
         else if (usedLanguage === 'TypeScript') {
-            color = `rgb(63,115, 135, ${opacity})`
+            color = `rgb(63, 115, ${135 + colorModifier}, ${opacity})`
         }
         else if (usedLanguage === 'CSS') {
-            color = `rgb(82,64, 120, ${opacity})`
+            color = `rgb(82, 64, ${120 + colorModifier}, ${opacity})`
         }
         else if (usedLanguage === 'Python') {
-            color = `rgb(68,114,161, ${opacity})`
+            color = `rgb(68, 114, ${161 + colorModifier}, ${opacity})`
         }
         else if (usedLanguage === 'SCSS') {
-            color = `rgb(184,91,138, ${opacity})`
+            color = `rgb(184, 91, ${138 + colorModifier}, ${opacity})`
         }
         else if (usedLanguage === null) {
-            color = `rgb(255,255,255, ${opacity})`
+            color = `rgb(255, 255, 255, ${opacity})`
         }
         return color;
     }
@@ -54,7 +54,7 @@ export const ApiCard = props => {
     const capitalized = split.map((e) => capitalizeFirstLetter(e));
     apiName = capitalized.join('-');
 
-    technologyUsedColor(usedLanguage, containerBackgroundOpacity);
+    technologyUsedColor(usedLanguage, 0, containerBackgroundOpacity);
 
     const [bgColorOnHover, setBgColorOnHover] = useState(initialColor);
 
@@ -82,7 +82,7 @@ export const ApiCard = props => {
             <div className={'api-card__lower'}>
                 <ul className={'api-card__link-list'}>
                     <li className={'api-card__link'} style={ghPages === null ? {display: 'none'} : {'--i': 1}}>
-                        <a href={ghPages=== null ? '' : ghPages}> <FontAwesomeIcon icon={faGithub}/> </a>
+                        <a href={ghPages === null ? '' : ghPages}> <FontAwesomeIcon icon={faGithub}/> </a>
                     </li>
                     {/*<li className={'api-card--link'} style={{'--i': 2}}>*/}
                     {/*    <a href="https://kwiatkowski1981.github.io/simple-editor/"> <FontAwesomeIcon icon={faYoutube}/>*/}
